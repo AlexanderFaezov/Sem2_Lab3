b58f36ee5f7e0182883b654215f08fed2b66c92f
AlexanderFaezov <s.faezov.com@mail.ru>
2024-02-05 19:15:46 +0500

    Добавьте файлы проекта.

---
+++ Sem2_Lab3/Sem2_Lab3.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    using namespace std;
+
+    const int n = 7;
+    int a[n];
+    int temp;
+    int k;
+
+    srand(time(0));
+
+    for (int i = 0; i < n; i++)
+    {
+        a[i] = rand() % 101 - 50;
+        cout << a[i] << " ";
+    }
+
+    cout << endl;
+    cout << "Введите число k: ";
+    cin >> k;
+
+    for (int i = 0; i < k; ++i) //сдвиг массива влево k раз
+    {
+        temp = a[0];
+        for (int j = 0; j < (n - 1); j++)
+        {
+            a[j] = a[j + 1];
+        }
+        a[n - 1] = temp; 
+    }
+
+    for (int i = 0; i < n; i++) //вывод массива
+    {
+        cout << a[i] << " ";
+    }
+}
